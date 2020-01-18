6b27c2e8fa1b752ee45986e0d27a1c3f64a7dfa0
Aidas Bendoraitis <aidasbend@yahoo.com>
2020-01-18 08:12:12 +0100

    🔨 Code improvements and enhancements for Chapter 11.

--- ch11/myproject_virtualenv/src/django-myproject/myproject/settings/test.py
+++ ch11/myproject_virtualenv/src/django-myproject/myproject/settings/test.py
@@ -1,4 +1,5 @@
 from ._base import *
+import structlog
 
 DEBUG = True
 
@@ -6,3 +7,31 @@ WEBSITE_URL = "http://127.0.0.1:8000"  # without trailing slash
 MEDIA_URL = f"{WEBSITE_URL}/media/"
 
 TESTS_SHOW_BROWSER = True
+
+LOGGING_CONFIG = None
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": True,
+    "handlers": {"null": {"class": "logging.NullHandler"}},
+    "loggers": {"django_structlog": {"handlers": ["null"], "level": "INFO"}},
+}
+
+structlog.configure(
+    processors=[
+        structlog.stdlib.filter_by_level,
+        structlog.processors.TimeStamper(fmt="iso"),
+        structlog.stdlib.add_logger_name,
+        structlog.stdlib.add_log_level,
+        structlog.stdlib.PositionalArgumentsFormatter(),
+        structlog.processors.StackInfoRenderer(),
+        structlog.processors.format_exc_info,
+        structlog.processors.UnicodeDecoder(),
+        structlog.processors.ExceptionPrettyPrinter(),
+        structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
+    ],
+    context_class=structlog.threadlocal.wrap_dict(dict),
+    logger_factory=structlog.stdlib.LoggerFactory(),
+    wrapper_class=structlog.stdlib.BoundLogger,
+    cache_logger_on_first_use=True,
+)
